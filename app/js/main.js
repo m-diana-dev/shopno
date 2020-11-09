681c0f6b71f8f77c8da7787d54a5e7e95faf88eb
m-diana-dev <m.diana.dev@gmail.com>
2020-11-09 23:28:24 +0300

    added header and about page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
+    $('.header__hamburger-menu').on('click', function(){
+        $('.header__menu-list').slideToggle();
+        $('.header__hamburger-menu-line').toggleClass('color-change');
+      });
 
+      $('.about-content__slider').slick({
+        arrows: false,
+        dots: true
+      });
 });
