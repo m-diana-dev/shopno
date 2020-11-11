2bc3d836949a7968844dce51c1ae0f8e65120654
m-diana-dev <m.diana.dev@gmail.com>
2020-11-11 19:19:11 +0300

    added section reviews, section clients, section contacts and footer

--- app/js/main.js
+++ app/js/main.js
@@ -16,4 +16,23 @@ $(function(){
       $('.close-btn').on('click', function(){
         $(this).parent().removeClass('active');
       });
+
+      $('.reviews-content__slider').slick({
+        arrows: false,
+        dots: true,
+        asNavFor: '.reviews-text__slider'
+      });
+      $('.reviews-text__slider').slick({
+        asNavFor: '.reviews-content__slider',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/prev.png" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/prev.png" alt=""></button>'
+      });
+
+      $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "14px",
+        readOnly: true,
+        ratedFill: "#f2da3b",
+        spacing: "7px"
+      });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,8 @@ gulp.task('sass', function(){
 gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css'
+        'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/rateyo/src/jquery.rateyo.css'
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -30,7 +31,8 @@ gulp.task('style', function(){
 
 gulp.task('script', function(){
     return gulp.src([
-        'node_modules/slick-carousel/slick/slick.js'
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/rateyo/src/jquery.rateyo.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
